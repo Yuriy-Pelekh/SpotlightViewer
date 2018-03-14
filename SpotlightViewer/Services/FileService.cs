aa52758370c9e86cf43276dd8228579dd44173e3
Yuriy Pelekh <ypelekh@softserveinc.com>
2018-03-14 12:51:40 +0200

    Temp commit.

---
+++ SpotlightViewer/Services/FileService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Storage;
+using Windows.System;
+
+namespace SpotlightViewer.Services
+{
+    public class FileService
+    {
+        public IEnumerable<string> GetFiles()
+        {
+            var r1 = KnownFolders.DocumentsLibrary;
+
+
+            var root = Path.GetTempPath();//Environment.ExpandEnvironmentVariables("%localappdata%");
+            var r = new DirectoryInfo(root);
+            r = r.Parent.Parent.Parent;
+            
+            const string directory = @"Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
+            
+            var path = Path.Combine(r.FullName, directory);
+
+            if (!Directory.Exists(path))
+            {
+                return Enumerable.Empty<string>();
+            }
+            var d = new DirectoryInfo(path);
+            var rr = d.GetFiles();
+            return rr.Select(file => file.FullName);
+        }
+    }
+}
---
+++ SpotlightViewer/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using SpotlightViewer.Services;
+
+namespace SpotlightViewer.ViewModels
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<string> Images { get; set; }
+
+        public MainViewModel(FileService fileService)
+        {
+            foreach (var filePath in fileService.GetFiles())
+            {
+                Images.Add(filePath);
+            }
+        }
+    }
+}
