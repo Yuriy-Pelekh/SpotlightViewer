cabe98d31e15b1d18249be5198610b727d0eea2b
Yuriy Pelekh <ypelekh@softserveinc.com>
2018-03-14 14:19:14 +0200

    Fixed files retrieving.

--- SpotlightViewer/Services/FileService.cs
+++ SpotlightViewer/Services/FileService.cs
@@ -9,21 +9,17 @@ namespace SpotlightViewer.Services
     {
         public IEnumerable<string> GetFiles()
         {
-            var root = Environment.ExpandEnvironmentVariables("%localappdata%");
-            var r = new DirectoryInfo(root);
-            r = r.Parent.Parent.Parent;
-            
+            var root = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
             const string directory = @"Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
-            
-            var path = Path.Combine(r.FullName, directory);
-
+            var path = Path.Combine(root, directory);
             if (!Directory.Exists(path))
             {
                 return Enumerable.Empty<string>();
             }
-            var d = new DirectoryInfo(path);
-            var rr = d.GetFiles();
-            return rr.Select(file => file.FullName);
+
+            var assets = new DirectoryInfo(path);
+            var files = assets.GetFiles();
+            return files.Select(file => file.FullName);
         }
     }
 }
